d51ac989b264ea7bfdbd47aba08dedbd53cfb850
Adrian Istrate <adrian.istrate@yahoo.com>
2010-07-02 17:24:20 +0200

    Add project RegexParser.Tests (NUnit tests).

---
+++ RegexParser.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace RegexParser.Tests
+{
+    [TestFixture]
+    public class CachedTest
+    {
+        [Test(Description = "Description of Test1.")]
+        public void Test1()
+        {
+
+        }
+    }
+}
