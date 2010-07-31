830897bcbe9c1b2ce36eecf43149dfddb133cd60
Adrian Istrate <adrian.istrate@yahoo.com>
2010-08-01 00:10:30 +0200

    Add (Haskell) Parsec non-determinism tests.

--- HaskellParsec/NonDeterminism.hs
+++ HaskellParsec/NonDeterminism.hs
@@ -1,16 +1,43 @@
 import Text.ParserCombinators.Parsec
 
 
-pattern1 = do x <- (string "a" <|> string "ab")
-              y <- string "bbbc"
-              return $ x ++ y
+patternA1 = do x <- (string "a" <|> string "ab")
+               y <- string "bbbc"
+               return $ x ++ y
 
-pattern2 = do x <- (string "ab" <|> string "a")
-              y <- string "bbbc"
-              return $ x ++ y
+patternA2 = do x <- (string "ab" <|> string "a")
+               y <- string "bbbc"
+               return $ x ++ y
+
+testAltern = do parseTest patternA1 "abbbc"
+                parseTest patternA2 "abbbbc"
+                parseTest patternA1 "abbbbc"
+                parseTest patternA2 "abbbc"
 
 
-main = do parseTest pattern1 "abbbc"
-          parseTest pattern2 "abbbbc"
-          parseTest pattern1 "abbbbc"
-          parseTest pattern2 "abbbc"
+patternB1 = string "abc" <|> string "xyz"
+
+patternB2 = string "xbc" <|> string "xyz"
+
+patternB3 = string "xyc" <|> string "xyz"
+
+patternB4 = try (string "xyc") <|> string "xyz"
+
+testPredictive = do parseTest patternB1 "xyz"
+                    parseTest patternB2 "xyz"
+                    parseTest patternB3 "xyz"
+                    parseTest patternB4 "xyz"
+
+
+patternC1 = do x <- ((try (string "a")) <|> string "ab")
+               y <- string "bbbc"
+               return $ x ++ y
+
+patternC2 = do x <- ((try (string "ab")) <|> string "a")
+               y <- string "bbbc"
+               return $ x ++ y
+
+testAltern2 = do parseTest patternC1 "abbbc"
+                 parseTest patternC2 "abbbbc"
+                 parseTest patternC1 "abbbbc"
+                 parseTest patternC2 "abbbc"
